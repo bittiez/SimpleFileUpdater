de505c28b1408dd2df82bf6c19a3ae179f8e4b93
TazmanianTad <bittiez@gmail.com>
2025-06-26 21:35:28 -0600

    Update start.sh

--- pyserver/start.sh
+++ pyserver/start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+PID=0
+
+cleanup() {
+    if [ $PID -ne 0 ]; then
+        kill -9 $PID 2>/dev/null
+        wait $PID 2>/dev/null
+    fi
+    exit 0
+}
+
+trap cleanup SIGINT SIGTERM
+
 while true; do
     python3 serv.py &
     PID=$!
